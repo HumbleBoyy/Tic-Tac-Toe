f4bc0c762f1715730060c68f183582c4ba46a04a
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-07-08 15:14:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TicTacToe from './Components/TicTac/TicTacToe'
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/images/images.js
@@ -0,0 +1,7 @@
+import circle from "../../assets/circle.png"
+import cross from "../../assets/cross.png"
+
+export default{
+    cross,
+    circle
+}
